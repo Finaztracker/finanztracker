44d8ca77ba0ece680196f32ed3761a5a7a52931a
fakeSimon <simonpoez@gmail.com>
2026-03-18 18:54:25 +0100

    push
    commit

--- src/main/java/de/finanztrackerproject/service/TransactionService.java
+++ src/main/java/de/finanztrackerproject/service/TransactionService.java
@@ -29,4 +29,42 @@ public class TransactionService {
 
         return balance;
     }
+
+    public double berechneEinnahmen() {
+        double total = 0;
+        for (Transaction t : transactions) {
+            if (t.getType().equals("Einkommen")) {
+                total += t.getAmount();
+            }
+        }
+        return total;
+    }
+
+
+    public double berechneAusgaben() {
+        double total = 0;
+        for (Transaction t : transactions) {
+            if (t.getType().equals("Ausgabe")) {
+                total += t.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public void zeigeTransaktion() {
+
+        if (transactions.isEmpty()) {
+            System.out.println("Keine Transaktionen vorhanden.");
+            return;
+        }
+
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+
+    }
+
+
+
+
 }
